bb7693830b004fc40b1d8622b86f594f3090ff33
algore87 <alexschott87@gmail.com>
2017-11-09 16:25:09 +0100

    Added Cors to backend.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 
 app.use(function (req, res, next) {
     console.log('Time:', Date.now());
